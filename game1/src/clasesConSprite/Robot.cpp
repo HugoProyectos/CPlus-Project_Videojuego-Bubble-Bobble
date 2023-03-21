7eb9245fe8e67f580ec8720939c39abb4b7a6226
HugoLazaro <801758@unizar.es>
2023-03-21 18:34:47 +0100

    Robot chan uwu

--- game1/src/clasesConSprite/Robot.cpp
+++ game1/src/clasesConSprite/Robot.cpp
@@ -4,14 +4,22 @@
 class Robot : public Enemigo {
 public:
     
+    //Sprite pixels
+    int pixels = 16;
+
     //Animation
     Texture2D walkAnimation = LoadTexture("resources/enemyRobot/robotWalk.png");
     Texture2D deadAnimation = LoadTexture("resources/enemyRobot/robotMuerte.png");
+    Texture2D animations[2] = { walkAnimation, deadAnimation };
+
     int fWalkAnimation = 4; //VECTOR(cada animacion tiene un numero de frames)       Número de fotogramas de la animacion 
+    int fDeadAnimation = 2;
+    int fAnimation[2] = { fWalkAnimation , fDeadAnimation };
+    
     int widthAnimation; //Se actualiza para cada animación activa
     int heightAnimation;
 
-    int animacionActiva = 0; //Indica la animación activa: 0->WalkAnimation
+    int animacionActiva = 0; //Indica la animación activa: 0->WalkAnimation, 1->DeadAnimation
     int indiceAnimacion = 0; //Indica el número de frame actual de la animación activa
 
     //Frames
@@ -46,6 +54,14 @@ public:
             case 0:
                 //Actualizar width&height animacion
                 indiceAnimacion = (indiceAnimacion + 1) % fWalkAnimation;
+                widthAnimation = walkAnimation.width / fWalkAnimation;
+                heightAnimation = walkAnimation.height;
+                break;
+            case 1:
+                //Actualizar width&height animacion
+                indiceAnimacion = (indiceAnimacion + 1) % fAnimation[1];
+                widthAnimation = deadAnimation.width / fAnimation[1];
+                heightAnimation = deadAnimation.height;
                 break;
             default:
                 break;
@@ -55,17 +71,18 @@ public:
 
     void Dibujar() {
         srcRec.x = (float)widthAnimation * (float)indiceAnimacion;
-        //Cambiar a un vector de animaciones[animacionActiva]
-        DrawTexturePro(walkAnimation, srcRec, destRec, origin, 0.0f, WHITE);
+        DrawTexturePro(animations[animacionActiva], srcRec, destRec, origin, 0.0f, WHITE);
     }
 
     //funciones de comportamiento
     void MoverIzq() {
         destRec.x -= velocidadLateral;
+        srcRec.width = pixels;
     }
 
     void MoverDer() {
         destRec.x += velocidadLateral;
+        srcRec.width = -pixels;
     }
 
     void Salto() {
