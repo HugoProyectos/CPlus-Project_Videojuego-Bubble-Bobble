7b566724da70e517230bb74d7be5791b4dfc44a6
newfeels <newfeelsproductions@gmail.com>
2023-04-13 17:15:20 +0200

    morado choca con pared

--- game1/src/clasesConSprite/Morado.cpp
+++ game1/src/clasesConSprite/Morado.cpp
@@ -337,6 +337,17 @@ public:
             cayendo = false;
         }
     }
+
+    void compruebaPared(const Columnas& s) override {
+        //Comprobamos columna derecha
+        if (s.left_der < (destRec.x + destRec.width / 2)) {
+            destRec.x = s.left_der - destRec.width / 2;
+        }
+        //Comprobamos columna izquierda
+        else if (s.right_izq > (destRec.x - destRec.width / 2)) {
+            destRec.x = s.right_izq + destRec.width / 2;
+        }
+    }
 };
 
 typedef std::shared_ptr<Morado> sh_Morado;
